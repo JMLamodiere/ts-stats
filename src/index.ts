8b47d603a411d4dbebec75cc3671d3f527b126e1
Jean-Marie Lamodière <j.lamodiere@meetic-corp.com>
2021-09-16 13:47:52 +0200

    feat: count games won by a team

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,20 @@ const matches = fs
     encoding: "utf-8",
   })
   .split("\n")
-  .map((row: string) => {
+  .map((row: string): string[] => {
     return row.split(",");
   });
 
 console.log(matches);
+
+let manUnitedWins = 0;
+
+for (let match of matches) {
+  if (match[1] === "Man United" && match[5] === "H") {
+    manUnitedWins++;
+  } else if (match[2] === "Man United" && match[5] === "A") {
+    manUnitedWins++;
+  }
+}
+
+console.log(`Man United win ${manUnitedWins} games`);
